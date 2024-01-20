bdcc2014b3fae058fcf54a27f090d4cad0e64cfd
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-01-19 22:31:59 -0500

    added switch to beginning do commands later

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/PropDetector.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.other;public class PropDetector {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/testing_teleops/VisionTesting.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.teleops.testing_teleops;public class VisionTesting {
+}
