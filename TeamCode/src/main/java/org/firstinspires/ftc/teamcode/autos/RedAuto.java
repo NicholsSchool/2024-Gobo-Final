576bd94c552e07e6c0ab670342856bcf893c4bb2
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-01-21 14:26:24 -0500

    end of qualifiers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedAuto.java
@@ -50,10 +50,14 @@ public class RedAuto extends LinearOpMode implements DriveConstants, ArmConstant
         lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GRAY);
 
 
-        double[][] points1 = new double[][]{{34.0, 38.8}, {55.2, 64.7}, {88.8, -53.6}, {-58.3, 11.5}};
+        double[][] points1 = new double[][]{{36.0, 38.0}, {43.0, 57.0}, {55.5, 33.6}, {49.0, 10.8}};
+        double[][] points2 = new double[][]{{49.0, 10.8}, {2.6, -0.1}, {-13.5, 8.9}, {-57.3, 5.0}};
 
         Spline spline1 = new Spline(points1, 20, drivetrain, 100);
         spline1.update();
+
+        Spline spline2 = new Spline(points2, 20, drivetrain, 100);
+        spline2.update();
 
         Recognition bestRec = null;
         int propPosition = 1;
@@ -81,18 +85,18 @@ public class RedAuto extends LinearOpMode implements DriveConstants, ArmConstant
             int point = (int) ((bestRec.getLeft() + bestRec.getRight()) / 2.0);
             if (point < 450.0) {
                 propZone = PropZones.CENTER;
-                purplePixelAngle = -75.0;
+                purplePixelAngle = -105;
             } else {
                 propZone = PropZones.RIGHT;
-                purplePixelAngle = -165.0;
+                purplePixelAngle = -125;
             }
         }
         else {
             propZone = PropZones.LEFT;
-            purplePixelAngle = -30.0;
+            purplePixelAngle = 0.0;
         }
 
-        double[] scanCoords = new double[]{36.0, 38.0};
+        double[] scanCoords = propZone.equals(PropZones.LEFT) ? new double[]{35.0, 38.0} : new double[]{36.0, 38.0};
         double distance = SPLINE_ERROR;
 
         drivetrain.setDesiredHeading(0.0);
@@ -163,6 +167,25 @@ public class RedAuto extends LinearOpMode implements DriveConstants, ArmConstant
         while(waitTime.time() < 1.0) {
             drivetrain.update();
             arm.armGoToPosition(-ARM_STARTING_POS);
+            arm.wristGoToPos(-WRIST_AUTO_POSITION);
+        }
+
+        if(propZone.equals(PropZones.RIGHT)) {
+            drivetrain.setDesiredHeading(-150.0);
+            double otherError = AUTO_ALIGN_ERROR;
+            while (otherError >= AUTO_ALIGN_ERROR) {
+                drivetrain.update();
+                double angle = drivetrain.getFieldHeading();
+                drivetrain.drive(0.0, 0.0, 0.0, true, true);
+                otherError = Math.abs(angle + 150.0);
+            }
+            drivetrain.drive(0, 0, 0, false, false);
+        }
+
+        waitTime.reset();
+        while(waitTime.time() < 1.0) {
+            drivetrain.update();
+            arm.wristGoToPos(-WRIST_AUTO_POSITION);
         }
 
         waitTime.reset();
@@ -179,7 +202,7 @@ public class RedAuto extends LinearOpMode implements DriveConstants, ArmConstant
 
         hand.rightGrabber(true);
 
-        while(spline1.desiredT() < 0.99) {
+        while(spline1.desiredT() < 0.98) {
             spline1.update();
             double[] robotPose = drivetrain.getXY();
 
@@ -201,14 +224,11 @@ public class RedAuto extends LinearOpMode implements DriveConstants, ArmConstant
 
             double desiredT = spline1.desiredT();
 
-            lowGear = desiredT < 0.6 || desiredT > 0.9;
-            if(desiredT > 0.8)
-                drivetrain.setDesiredHeading(-180.0);
-            else
-                drivetrain.setDesiredHeading(0.0);
+            lowGear = desiredT < 0.7 || desiredT > 0.8;
+            drivetrain.setDesiredHeading(0.0);
 
             drivetrain.drive(power1 * Math.cos(spline1.angle()), power1 * Math.sin(spline1.angle()), turn, autoAlign, lowGear);
-            if (sampleTime.time() > 30) {
+            if (sampleTime.time() > 20) {
                 spline1.update();
                 sampleTime.reset();
             }
@@ -219,8 +239,62 @@ public class RedAuto extends LinearOpMode implements DriveConstants, ArmConstant
             telemetry.addData("bezierY", spline1.bezierY(spline1.desiredT()));
             telemetry.update();
         }
+
+        while(spline2.desiredT() < 0.98) {
+            spline2.update();
+            double[] robotPose = drivetrain.getXY();
+
+            hand.leftGrabber(true);
+            hand.rightGrabber(true);
+
+            double jamesSplineError = Math.hypot(robotPose[0] - points2[3][0], robotPose[1] - points2[3][1]);
+            double power2 = 0.0;
+            if(jamesSplineError > 2 * SPLINE_ERROR)
+                power2 = Range.clip(
+                        SPLINE_P * Math.hypot(robotPose[0] - points2[3][0], robotPose[1] - points2[3][1]), 0.0, 1.0);
+            else
+                break;
+
+            boolean lowGear;
+
+            double turn = 0;
+            boolean autoAlign = true;
+
+            double desiredT = spline2.desiredT();
+
+            lowGear = desiredT < 0.7 || desiredT > 0.8;
+            if(desiredT > 0.77)
+                drivetrain.setDesiredHeading(-180.0);
+            else
+                drivetrain.setDesiredHeading(0.0);
+
+            drivetrain.drive(power2 * Math.cos(spline2.angle()), power2 * Math.sin(spline2.angle()), turn, autoAlign, lowGear);
+            if (sampleTime.time() > 20) {
+                spline2.update();
+                sampleTime.reset();
+            }
+            telemetry.addData("x", drivetrain.getXY()[0]);
+            telemetry.addData("y", drivetrain.getXY()[1]);
+            telemetry.addData("t", spline1.desiredT());
+            telemetry.addData("bezierX", spline1.bezierX(spline1.desiredT()));
+            telemetry.addData("bezierY", spline1.bezierY(spline1.desiredT()));
+            telemetry.update();
+        }
+
         drivetrain.drive(0, 0, 0, false, false);
         hand.leftGrabber(false);
+
+        drivetrain.setDesiredHeading(-180.0);
+        double anotherError = AUTO_ALIGN_ERROR;
+        while (anotherError >= AUTO_ALIGN_ERROR) {
+            drivetrain.update();
+            double angle = drivetrain.getFieldHeading();
+            drivetrain.drive(0.0, 0.0, 0.0, true, true);
+            anotherError = Math.abs(angle + 180.0);
+        }
+        drivetrain.drive(0, 0, 0, false, false);
+
+
         terminateOpModeNow();
     }
 }
